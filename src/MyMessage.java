94e7a2ead49720ec57b812c6598f26beceb33454
Valentin666 <lita-stroy@mail.ru>
2018-11-18 07:58:40 +0500

    GitBranch

--- src/MyMessage.java
+++ src/MyMessage.java
@@ -1,5 +1,6 @@
 public class MyMessage {
     public static void main(String[] args) {
         System.out.println("MyMessage!");
+        System.out.println("Создал две новые ветки гита!");
     }
 }
