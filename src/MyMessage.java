08851544408f232ce455dbf5728ad9304fc2c70e
Valentin666 <lita-stroy@mail.ru>
2018-11-18 08:23:03 +0500

    MyMessage3

--- src/MyMessage.java
+++ src/MyMessage.java
@@ -3,5 +3,6 @@ public class MyMessage {
         System.out.println("MyMessage!");
         System.out.println("Создал две новые ветки гита!");
         System.out.println("MyMessage2!");
+        System.out.println("MyMessage3!");
     }
 }
