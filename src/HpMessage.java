d51b1d786ab4c9d67e5079f0cc5851fbf06beb23
Valentin Mironenko <lita-stroy@mail.ru>
2018-11-18 08:24:21 +0500

    HpMessage2

--- src/HpMessage.java
+++ src/HpMessage.java
@@ -2,5 +2,6 @@ public class HpMessage {
     public static void main(String[] args) {
         System.out.println("HpMessage!");
         System.out.println("Hp2!");
+        System.out.println("HpMessage2!");
     }
 }
